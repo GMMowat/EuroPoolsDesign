b357548fc429e4930355524190bea0b60783a81d
Graham Mowat <graham.m.mowat@gmail.com>
2017-02-09 23:38:53 +0000

    Created a mobile friendly menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
 //The beginning of our scripts.js file which will contain any code affecting functionality of the site
+$(document).ready(function(){
+
+  $(".open-menu").click(function(){
+    $(".side-nav").css("width", "100%");
+  });
+
+  $(".close-menu").click(function(){
+    $(".side-nav").css("width", "0%");
+  });
+
+});
