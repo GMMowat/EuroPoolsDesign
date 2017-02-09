2aff13eb54aef6ada96a3ca425e093b98a7993eb
Graham Mowat <graham.m.mowat@gmail.com>
2017-02-09 23:42:34 +0000

    Added some test content to the site

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,12 @@ $(document).ready(function(){
 
   $(".open-menu").click(function(){
     $(".side-nav").css("width", "100%");
+    $(".open-menu").hide();
   });
 
   $(".close-menu").click(function(){
     $(".side-nav").css("width", "0%");
+    $(".open-menu").show();
   });
 
 });
