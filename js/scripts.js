2968a40382ab70987617259cecace570882be75a
Graham Mowat <graham.m.mowat@gmail.com>
2017-02-10 21:36:39 +0000

    Possible completion of mobile menu pending improvements on functionality or ideas. Added a navbar which reacts to scrolling and fixes itself to the top of the page with some animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,20 @@
 //The beginning of our scripts.js file which will contain any code affecting functionality of the site
-$(document).ready(function(){
+$(window).scroll(function(){
+  var sticky = $(".header"),
+      scroll = $(window).scrollTop();
 
+  if (scroll > 0) sticky.addClass("fixed");
+  else sticky.removeClass("fixed");
+});
+
+$(document).ready(function(){
   $(".open-menu").click(function(){
     $(".side-nav").css("width", "100%");
-    $(".open-menu").hide();
+    $(".blur").css("filter", "blur(5px)");
   });
 
   $(".close-menu").click(function(){
     $(".side-nav").css("width", "0%");
-    $(".open-menu").show();
+    $(".blur").css("filter", "blur(0px)");
   });
-
 });
