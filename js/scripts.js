c78b310867fc108fe29596fcf34b1f04c7087eea
Graham Mowat <graham.m.mowat@gmail.com>
2017-02-11 00:17:34 +0000

    Made the new branch for mobile menu change

--- js/scripts.js
+++ js/scripts.js
@@ -9,12 +9,10 @@ $(window).scroll(function(){
 
 $(document).ready(function(){
   $(".open-menu").click(function(){
-    $(".side-nav").css("width", "100%");
-    $(".blur").css("filter", "blur(5px)");
+    $(".side-nav").css("max-height", "100%");
   });
 
   $(".close-menu").click(function(){
-    $(".side-nav").css("width", "0");
-    $(".blur").css("filter", "blur(0px)");
+    $(".side-nav").css("max-height", "0");
   });
 });
