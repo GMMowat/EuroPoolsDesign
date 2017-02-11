8dc4c97d0e8026589003545df0aee0e7ec7320e3
Graham Mowat <graham.m.mowat@gmail.com>
2017-02-11 17:39:21 +0000

    Made some changes to the css and JavaScript to set how the mobile and desktop website menus react

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 //The beginning of our scripts.js file which will contain any code affecting functionality of the site
+
+//If the user scrolls down lock the navigation bar to the top of the window.
 $(window).scroll(function(){
   var sticky = $(".header"),
       scroll = $(window).scrollTop();
@@ -7,16 +9,37 @@ $(window).scroll(function(){
   else sticky.removeClass("fixed");
 });
 
+//Check if the website is ready
 $(document).ready(function(){
+  //menuOpen variable used when opening and closing mobile menu
    menuOpen = false;
+   //user clicks the open-menu button
     $(".open-menu").click(function(){
+      //if the menu isn't open run the code below
       if(menuOpen == false) {
+        //set the menuOpen variable to true because the menu is now open
         menuOpen = true;
+        //Change the height of the drop-nav so that it shows on screen
         $(".drop-nav").css("max-height", "100%");
+        //set a transition value so that the menu drops down in an animation
+        $(".drop-nav").css("transition", "1s");
+        //After a certain amount of time remove the transition value so that the drop-nav doesn't animate anymore
+        setTimeout(function () {
+          $('.drop-nav').css("transition", "initial");
+        }, 500);
       }
+      //if button is clicked again
       else {
+        //set menuOpen to false because the menu is closed
         menuOpen = false;
+        //hide the menu by clearing the height
         $(".drop-nav").css("max-height", "0");
+        //add a new transition value to animate the closing of the menu
+        $(".drop-nav").css("transition", "1s");
+        //After a certain amount of time remove the transition value so that the drop-nav doesn't animate anymore
+        setTimeout(function () {
+          $('.drop-nav').css("transition", "initial");
+        }, 500);
       }
     });
 });
